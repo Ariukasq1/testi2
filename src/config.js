c4b977f0e831bafc7e778ea7baf21b6ccc41a670
Gift_zwerg3 <bulgantamir@nmma.co>
2021-12-09 16:11:12 +0800

    update

--- src/config.js
+++ src/config.js
@@ -1,8 +1,4 @@
-import axios from "axios";
-
 export const Config = {
   apiUrl: "https://source.mn/wp/wp-json",
   menuUrl: "https://source.mn/wp/wp-json/menus/v1/menus",
 };
-
-export const fetcher = (url) => axios.get(url).then((res) => res.data);
--- src/utils.js
+++ src/utils.js
@@ -1,31 +1,37 @@
+import axios from "axios";
+
+export const fetcher = (url) => axios.get(url).then((res) => res.data);
+
 export const getData = (object, type) => {
   switch (type) {
-    case 'categories':
-      return (object &&
-        object['wp:term'] &&
-        object['wp:term'][0]) ? object['wp:term'][0] : [];
-    case 'tags':
-      return (object &&
-        object['wp:term'] &&
-        object['wp:term'][1]) ? object['wp:term'][1] : [];
-    case 'author':
-      return (object &&
-        object['author'] &&
-        object['author'][0]) ? object['author'][0] : null;
-    case 'image':
-      return (object &&
-        object['wp:featuredmedia'] &&
-        object['wp:featuredmedia'][0] &&
-        object['wp:featuredmedia'][0].source_url) ? object['wp:featuredmedia'][0].source_url : null;
+    case "categories":
+      return object && object["wp:term"] && object["wp:term"][0]
+        ? object["wp:term"][0]
+        : [];
+    case "tags":
+      return object && object["wp:term"] && object["wp:term"][1]
+        ? object["wp:term"][1]
+        : [];
+    case "author":
+      return object && object["author"] && object["author"][0]
+        ? object["author"][0]
+        : null;
+    case "image":
+      return object &&
+        object["wp:featuredmedia"] &&
+        object["wp:featuredmedia"][0] &&
+        object["wp:featuredmedia"][0].source_url
+        ? object["wp:featuredmedia"][0].source_url
+        : null;
     default:
       break;
   }
 };
 
 export const prefixer = (url) => {
-  if (process.env.NODE_ENV !== 'production') {
+  if (process.env.NODE_ENV !== "production") {
     return url;
   }
 
   return `/${url}`;
-}
+};
