6feba086128f0d8f799ba672f8cf06148d6d77c9
Gift_zwerg3 <bulgantamir@nmma.co>
2021-12-15 21:20:37 +0800

    update

---
+++ src/components/about/AboutPost.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { getData } from "../../utils";
+import Link from "next/link";
+
+const AboutPost = ({ post }) => {
+  const { title, content, _embedded } = post;
+  return (
+    <div className="about-post">
+      <div className="post-image">
+        <img src={getData(_embedded, "image")} />
+      </div>
+      <div className="post-text">
+        <h2 dangerouslySetInnerHTML={{ __html: title.rendered }} />
+        <div dangerouslySetInnerHTML={{ __html: content.rendered }} />
+      </div>
+    </div>
+  );
+};
+
+export default AboutPost;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,30 @@
 import React from "react";
+import Footer from "../components/layouts/footer";
+import WPAPI from "wpapi";
+import Config from "../config";
+import AboutPost from "../components/about/AboutPost";
 
-const About = () => {
-  return <div className="page About">hello</div>;
+const wp = new WPAPI({ endpoint: Config.apiUrl });
+
+const About = ({ contact, posts }) => {
+  return (
+    <div className="page About">
+      <AboutPost post={posts[0]} />
+      <Footer contact={contact[0]} />
+    </div>
+  );
 };
+
+export async function getStaticProps() {
+  const contact = await wp.posts().categories().slug("contact").embed();
+  const posts = await wp.posts().categories(207).embed();
+
+  return {
+    props: {
+      contact,
+      posts,
+    },
+  };
+}
+
 export default About;
